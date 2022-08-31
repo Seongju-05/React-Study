5f4dbc489648bd8ba68383c3c5a6767d50d600c4
Sswmhskr06ksj <81690443+Sswmhskr06ksj@users.noreply.github.com>
2022-08-31 20:10:45 +0900

    2022-08-31
    React 실습

---
+++ Blog/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="balck-nav">
+        <h1>Blog</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
