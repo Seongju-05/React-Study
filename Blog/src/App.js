a679f9c1f7e48f3024ffd377caaf586c0e4366db
Sswmhskr06ksj <81690443+Sswmhskr06ksj@users.noreply.github.com>
2022-09-01 22:03:55 +0900

    2022-09-01
    React 블로그 좋아요 숫자 올리기 성공

--- Blog/src/App.js
+++ Blog/src/App.js
@@ -2,11 +2,6 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-let post = "대소고 학생";
-let [글제목, a] = useState("대소고 추천");
-let [글제목2, b] = useState("대소고란");
-let [글제목3, c] = useState("대소고 기숙사");
-
 /*
 // Destructuring 문법
 let num = [1, 2];
@@ -14,21 +9,46 @@ let [a, c] = [1, 2];
 */
 
 function App() {
+  let post = "대소고 학생";
+  let [글제목, 글제목변경] = useState(["대소고", "대소고란", "대소고 급식"]);
+  let [좋아요, 좋아요변경] = useState(0);
+
   return (
     <div className="App">
-      <div className="balck-nav">
-        <h4>대소로그</h4>
+      <div className="black-nav">
+        <h1>대소로그</h1>
+      </div>
+      <button
+        onClick={() => {
+          console.log("클릭");
+          let copy = [...글제목];
+          copy[0] = "개소고";
+          copy.sort();
+          글제목변경(copy);
+        }}
+      >
+        글 정렬
+      </button>
+      <div className="list">
+        <h4>
+          {글제목[0]}
+          <span
+            onClick={() => {
+              좋아요변경(좋아요 + 1);
+            }}
+          >
+            👍
+          </span>
+          {좋아요}
+        </h4>
+        <p>8월 31일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목}</h4>
+        <h4>{글제목[1]}</h4>
         <p>8월 31일 발행</p>
       </div>
-      <div>
-        <h4>{글제목2}</h4>
-        <p>8월 31일 발행</p>
-      </div>
-      <div>
-        <h4>{글제목3}</h4>
+      <div className="list">
+        <h4>{글제목[2]}</h4>
         <p>8월 31일 발행</p>
       </div>
     </div>
