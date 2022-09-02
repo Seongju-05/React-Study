278e3a9b681c45b898e9aceeeea580bc231f5a59
Sswmhskr06ksj <81690443+Sswmhskr06ksj@users.noreply.github.com>
2022-09-02 12:34:28 +0900

    2022-09-02
    React 블로그 모달 만들기

--- Blog/src/App.js
+++ Blog/src/App.js
@@ -12,6 +12,7 @@ function App() {
   let post = "대소고 학생";
   let [글제목, 글제목변경] = useState(["대소고", "대소고란", "대소고 급식"]);
   let [좋아요, 좋아요변경] = useState(0);
+  let [modal, setmodal] = useState(false);
 
   return (
     <div className="App">
@@ -48,9 +49,26 @@ function App() {
         <p>8월 31일 발행</p>
       </div>
       <div className="list">
-        <h4>{글제목[2]}</h4>
+        <h4
+          onClick={() => {
+            setmodal(!modal);
+          }}
+        >
+          {글제목[2]}
+        </h4>
         <p>8월 31일 발행</p>
       </div>
+      {modal == true ? <Modal /> : null}
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
